cee16d8cea2bd7fbd17e6286b2e64c669881edfa
Marko Gelev <marko.gelev@students.ukim.finki.com.mk>
2020-04-12 17:25:26 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,17 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 	List<Integer> labPoints;
-	//TODO constructor
-
-	//TODO seters & getters
+	Student(String index,String firstName,String lastName)
+	{
+		this.index=index;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		labPoints = new ArrayList<Integer>();
+	}
+	public String getIndex()
+	{
+		return index;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public void printLabPoints()
+	{
+		for(int i=0;i<labPoints.size();i++)
+			System.out.println(labPoints.get(i));
+	}
+	public void setIndex(String index)
+	{
+		this.index = index;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName=firstName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName=lastName;
+	}
+	public void addLabPoints(Integer labPoint)
+	{
+		labPoints.add(labPoint);
+	}
 
 	public double getAverage() {
-		//TODO
+		double labPointsSum =0;
+		for(int i=0;i<labPoints.size();i++)
+			labPointsSum+=labPoints.get(i);
+		return labPointsSum/labPoints.size();
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if(labPoints.size()>=8)
+			return true;
+		else
+			return false;
 	}
 }
